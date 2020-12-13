dd33f08d41289d03c905bec1d3f898012ad7f6e3
Adam Zhu <adam@adamz.hu>
2020-12-12 19:16:02 -0500

    remove inlined testUser data

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const testUser = {
-  email: 'adam@adamzhu.co',
-  email_verified: true,
-  family_name: 'Zhu',
-  given_name: 'Adam',
-  locale: 'en',
-  name: 'Adam Zhu',
-  nickname: 'adam',
-  picture:
-    'https://lh3.googleusercontent.com/a-/AOh14Ghz1bjxC4Oy8ItgY51PqovayzJn23SG28qoIykiJA=s96-c',
-  sub: 'google-oauth2|101808654973584632667',
-  updated_at: '2020-12-12T17:54:05.765Z',
-};
+// const testUser = {
+//   email: 'adam@adamzhu.co',
+//   email_verified: true,
+//   family_name: 'Zhu',
+//   given_name: 'Adam',
+//   locale: 'en',
+//   name: 'Adam Zhu',
+//   nickname: 'adam',
+//   picture:
+//     'https://lh3.googleusercontent.com/a-/AOh14Ghz1bjxC4Oy8ItgY51PqovayzJn23SG28qoIykiJA=s96-c',
+//   sub: 'google-oauth2|101808654973584632667',
+//   updated_at: '2020-12-12T17:54:05.765Z',
+// };
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App user={testUser} />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <App user={testUser} />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
